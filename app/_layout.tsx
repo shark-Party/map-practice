9b7a5483b74cfc1f4be83c39d9b0d90ce09b87f5
Fynn <brickmaster2004@gmail.com>
2026-03-13 13:04:49 -0400

    changes being made

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  // useEffect
+  useEffect(() => {
+    const responseListner = Notification
+  })
   return (
     <Stack screenOptions={{headerShown: false}}>
       <Stack.Screen name="(exercise-tabs)" />
